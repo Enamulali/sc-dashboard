55c919d5cbe67b0f62179745597b22617a6dc0b8
Enamulali <enamul97@icloud.com>
2024-12-05 16:39:01 +0000

    setup theme/dark mode

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { colourModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
 function App() {
-  return <div className="app"></div>;
+  const [theme, colourMode] = useMode();
+
+  return (
+    <colourModeContext.Provider value={colourMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content"></main>
+        </div>
+      </ThemeProvider>
+    </colourModeContext.Provider>
+  );
 }
 
 export default App;
